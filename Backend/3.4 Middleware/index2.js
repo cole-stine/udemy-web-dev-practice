ac011021a59cc92a536952ceae1d8267b526262e
Cole Stine <cstine1234@gmail.com>
2024-04-16 17:25:19 -0700

    Initial Commit For New Repo

---
+++ Backend/3.4 Middleware/index2.js
@@ -0,0 +1,20 @@
+import express from "express";
+import morgan from "morgan"
+
+const app = express();
+const port = 3000;
+
+app.use(morgan("short"));
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.post("/user", (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
